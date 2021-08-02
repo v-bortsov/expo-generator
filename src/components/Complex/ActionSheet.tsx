9174d342b662c98045e0f629bff1d5d14faaca8e
Bortsov Vitaliy <6622319@gmail.com>
2021-08-02 09:21:06 +0300

    :bettle: fix(components): Add list a localizations

--- src/components/Complex/ActionSheet.tsx
+++ src/components/Complex/ActionSheet.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import { Button, Actionsheet, useDisclose, Icon, IconButton } from 'native-base';
 import { MaterialIcons } from '@expo/vector-icons';
+
+const languages = [
+  {
+    name: 'Русский', arrb: 'ru'
+  }, {
+    name: 'English', abbr: 'en'
+  }, {
+    name: 'Deutsche', abbr: 'de'
+  }, {name: 'Français', abbr: 'fr'}
+]
 export default function ActionSheet () {
   const { isOpen, onOpen, onClose } = useDisclose();
   return (
     <>
       <IconButton icon={<Icon onPress={onOpen} as={<MaterialIcons name='more-vert' />} size='sm' color="white" />} />
-      {/* <Button onPress={onOpen}>Actionsheet</Button> */}
-
       <Actionsheet isOpen={isOpen} onClose={onClose}>
         <Actionsheet.Content>
-          {/* <Actionsheet.Header>Header</Actionsheet.Header> */}
-          <Actionsheet.Item>Русский</Actionsheet.Item>
-          <Actionsheet.Item>English</Actionsheet.Item>
-          <Actionsheet.Item>Deutsche</Actionsheet.Item>
-          <Actionsheet.Item>Français</Actionsheet.Item>
-          <Actionsheet.Item color="red.500">Delete</Actionsheet.Item>
+          {languages.map(item=><Actionsheet.Item>{item.name}</Actionsheet.Item>)}
         </Actionsheet.Content>
-        {/* <Actionsheet.Footer>
-          <Actionsheet.Item onPress={onClose}>Cancel</Actionsheet.Item>
-        </Actionsheet.Footer> */}
       </Actionsheet>
     </>
   );
